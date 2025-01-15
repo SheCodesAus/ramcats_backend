166ef288409bbb91b39117db81f4f325c38ef770
Amber <amber.quynh.nguyen@gmail.com>
2025-01-15 14:49:10 +0800

    listings model drafted

--- furfuture/furfuture/settings.py
+++ furfuture/furfuture/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
+    'users.apps.UsersConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
